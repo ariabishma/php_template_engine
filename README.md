33d3193d57002e9525605702b8e5aebef0092a80
ariabishma <ariabishma848@gmail.com>
2018-10-31 19:51:19 +0700

    add readme.md with simple documentation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # php_template_engine
 simple php templating engine (.te.php) file extension
+
+ -- make file in template folder with (.te.php) extension
+ -- file structure like file in template/test.te.php
+ -- edit index file with data (array) and render (your template file)
--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 require_once 'core/TemplateEngine.php';
+
 use Core\TemplateEngine;
 
+// initialitaion class of TemplateEngine
 $te = new TemplateEngine;
+
+//initial data and template file
 $data = $te->data([
   'nama' => 'm.aria bishma',
   'kelas' => '11 RPL 1',
   'data' => array("nama"=>"bishma")
   ])->render('test');
-
 
 ?>
